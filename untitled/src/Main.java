074626e028b2261aae5bd45d1f9e74eca3e6d5c7
Andries Nieuwenhuize <nieu0082@hz.nl>
2024-10-24 11:00:35 +0200

    solution

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -6,11 +6,17 @@ public class Main {
         try {
 
             // Here, we have a player class and a ball.
-            Player player;
-            PlasticBall40mm ball;
+            Player player1 = new Player("Ping");
+            Player player2 = new Player("Pong");
+
+            PlasticBall40mm ball = new PlasticBall40mm();
 
             // The game needs two players who face each other.
             // Good luck!
+            player2.setOpponent(player1);
+            player1.setOpponent(player2);
+            
+            player1.start(ball);
 
         }catch (StackOverflowError ex) {
             Console.write("Game over");
